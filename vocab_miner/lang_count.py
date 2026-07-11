3061fec03cb186dbbd4e103869faecf1955bb4fa
hunterrowens4-arch <hunterrowens4@gmail.com>
2026-07-11 15:46:05 +0000

    refractor

--- vocab_miner/lang_count.py
+++ vocab_miner/lang_count.py
@@ -21,6 +21,42 @@ transcripts = {
     ],
 }
 
+def add_known_words():
+    language = input('\nWhich Known Words list would you like to add to?\n>> ').lower().strip()
+
+    # try to open the Known Words list for the language choice, if it fails, create a new Known Words list for that language
+    try:
+        known_words = load_known_words(language)
+    except FileNotFoundError:
+        known_words = []
+
+    # prompt user for words to add to the Known Words list, standardize input, and write the updated list to file
+    add_words_str = input('\nWhat words would you like to add to the list? Separate the words only with spaces.\n>> ').lower().strip()
+    add_words = [add_words_str.split()]
+    known_words = extend_list(known_words, add_words)
+
+    # try to copy the existing Known Words list to a backup file, if it fails, print a message that the Known Words list is being created, then write the updated Known Words list to file
+    copy_words_list("known_words", language)
+    save_words_list("known_words", known_words, language)
+
+def add_stop_words():
+    language = input('\nWhich Stop Words list would you like to add to?\n>> ').lower().strip()
+
+    # try to open the Stop Words list for the language choice, if it fails, create a new Stop Words list for that language
+    try:
+        stop_words = load_stop_words(language)
+    except FileNotFoundError:
+        stop_words = []
+
+    # prompt user for words to add to the Stop Words list, standardize input, and write the updated list to file
+    add_words_str = input('\nWhat words would you like to add to the list? Separate the words only with spaces.\n>> ').lower().strip()
+    add_words = [add_words_str.split()]
+    stop_words = extend_list(stop_words, add_words)
+
+    # try to copy the existing Stop Words list to a backup file, if it fails, print a message that the Stop Words list is being created, then write the updated Stop Words list to file
+    copy_words_list("stop_words", language)
+    save_words_list("stop_words", language)
+
 def analyze_transcript(text, stop_words, known_words):
     words = re.findall(r"\b\w+(?:'\w+)*\b", text, flags=re.UNICODE)
     lines = text.split("\n")
@@ -43,6 +79,22 @@ def analyze_transcript(text, stop_words, known_words):
 
     return results, counts
 
+def copy_words_list(words_list, language):
+    try:
+        shutil.copy(f'filters/{words_list}/{words_list}_{language}.txt',
+                    f'filters/{words_list}/backups/{words_list}_{language}_backup.txt')
+        print(f'\nCreating backup of {words_list}_{language}.txt')
+    except:
+        print(f'Creating {words_list}_{language}.txt')
+
+def extend_list(word_list, words):
+    for word in words:
+        if word in word_list:
+            continue
+    word_list.extend(word)
+    word_list.sort()
+    return word_list
+
 def get_transcript_request():
     # RANDOM FUNCTION DISABLED FOR REFRACTORING
     # QUIT FUNCTION DISABLED FOR REFRACTORING
@@ -55,6 +107,24 @@ def get_transcript_request():
     ).lower().strip()
 
     return filename, language
+
+def load_known_words(language):
+    try:
+        with open(f'filters/known_words/known_words_{language}.txt', 'r', encoding='utf-8') as f:
+            known_words = [line.strip().lower()
+                            for line in f if line.strip()]
+        return known_words
+    except FileNotFoundError:
+        raise
+
+def load_stop_words(language):
+    try:
+        with open(f'filters/stop_words/stop_words_{language}.txt', 'r', encoding='utf-8') as f:
+            stop_words = [line.strip().lower()
+                            for line in f if line.strip()]
+        return stop_words
+    except FileNotFoundError:
+        raise
 
 def load_transcript(filename, language):
     try:
@@ -82,11 +152,9 @@ def mine_transcript():
         # try to open the stop words list for the language choice, if it fails, prompt user to continue or return to main menu
 
         try:
-            with open(f'filters/stop_words/stop_words_{language}.txt', 'r', encoding='utf-8') as f:
-                stop_words = [line.strip().lower()
-                                for line in f if line.strip()]
+            stop_words = load_stop_words(language)
             break
-        except:
+        except FileNotFoundError:
             print(
                 '\nWARNING: No Stop Words list was found in that language choice. Continue?')
             continue_choice = input(
@@ -95,8 +163,6 @@ def mine_transcript():
                 break
             elif continue_choice == 'y':
                 break
-        if continue_choice == 'n':
-            continue
 
     # loop to get a valid response from the user regarding whether or not to use a Known Words filter, including option to quit to main menu
 
@@ -106,24 +172,19 @@ def mine_transcript():
         if filter_list == 'y':
 
             # try to open the Known Words list for the language choice, if it fails, prompt user to continue without a Known Words list, try again, or return to main menu
-
             try:
-                with open(f'filters/known_words/known_words_{language}.txt', 'r', encoding='utf-8') as f:
-                    known_words = [line.strip().lower()
-                                    for line in f if line.strip()]
-                    break
-            except:
-                print(
-                    'WARNING: No Known Words list was found for that language choice.\nContinuing without a Known Words list.')
-                known_words = []
+                known_words = load_known_words(language)
                 break
+            except FileNotFoundError:
+                print('\nError, File not found, try again.')
+            
         elif filter_list == 'n':
             known_words = []
             break
         elif filter_list == 'quit':
             break
         else:
-            print('Invalid input. Please respond with "y" or "n".')
+            print('Invalid input. Please respond with "y", "n", or "quit".')
         if filter_list == 'quit':
             continue
 
@@ -137,8 +198,11 @@ def mine_transcript():
 
     print(f'\nYou have identified {len(results)} study words out of {len(counts)} total words! ({len(results) / len(counts):.1%})')
 
-skip_choice = None
-
+def save_words_list(list_name, words_list, language):
+    with open(f'filters/{list_name}/{list_name}_{language}.txt', 'w', encoding='utf-8') as f:
+        for word in words_list:
+            f.write(f'{word}\n')
+    print(f'The list is now {len(words_list)} words long.')
 
 print('Welcome to the Vocab Miner!')
 
@@ -156,81 +220,21 @@ What would you like to do?
 >> """).lower().strip()
 
 # allow for language-agnostic vocab mining, including option to quit to main menu
-
     if choice == 'mine':
         mine_transcript()
        
 # allow for adding of words to a known words list, including input standardization and option to quit to main menu
     elif choice == 'add':
-        language = input('\nWhich Known Words list would you like to add to?\n>> ').lower().strip()
-
-    # try to open the Known Words list for the language choice, if it fails, create a new Known Words list for that language
-        try:
-            with open(f'filters/known_words/known_words_{language}.txt', 'r', encoding='utf-8') as f:
-                known_words = [line.strip().lower()
-                               for line in f if line.strip()]
-        except:
-            known_words = []
-
-    # prompt user for words to add to the Known Words list, standardize input, and write the updated list to file
-        add_words_str = input('\nWhat words would you like to add to the list? Separate the words only with spaces.\n>> ').lower().strip()
-        add_words = [add_words_str.split()]
-        for word in add_words:
-            if word in known_words:
-                continue
-            known_words.extend(word)
-            known_words.sort()
-
-    # try to copy the existing Known Words list to a backup file, if it fails, print a message that the Known Words list is being created, then write the updated Known Words list to file
-        try:
-            shutil.copy(f'filters/known_words/known_words_{language}.txt',
-                        f'filters/known_words/backups/known_words_{language}_backup.txt')
-            print(f'\nCreating backup of known_words_{language}.txt')
-        except:
-            print(f'Creating known_words_{language}.txt')
-        with open(f'filters/known_words/known_words_{language}.txt', 'w', encoding='utf-8') as f:
-            for word in known_words:
-                f.write(f'{word}\n')
-        print(f'"{add_words_str}" {'has' if len(add_words) == 1 else 'have'} successfully been added to known_words_{language}.txt')
-        print(f'The list is now {len(known_words)} words long.')
+        add_known_words()
 
 # all for adding words to stop words list from within the program
     elif choice == 'stop':
-        language = input("""
-What stop words list would you like to add to?
-Please enter the full language name or "done" to return to the menu.
->> """).lower().strip()
-    # try to open the Known Words list for the language choice, if it fails, create a new Known Words list for that language
-        try:
-            with open(f'filters/stop_words/stop_words_{language}.txt', 'r', encoding='utf-8') as f:
-                stop_words = [line.strip().lower() for line in f if line.strip()]
-        except:
-            known_words = []
-    # prompt user for words to add to the Stop Words list, standardize input, and write the updated list to file
-        add_words_str = input('\nWhat words would you like to add to the list? Separate the words only with spaces.\n>> ').lower().strip()
-        add_words = [add_words_str.split()]
-        for word in add_words:
-            if word in stop_words:
-                continue
-            stop_words.extend(word)
-            stop_words.sort()
-    # try to copy the existing Stop Words list to a backup file, if it fails, print a message that the Stop Words list is being created, then write the updated Stop Words list to file
-        try:
-            shutil.copy(f'filters/stop_words/stop_words_{language}.txt',
-                        f'filters/stop_words/backups/stop_words_{language}_backup.txt')
-            print(f'\nCreating backup of stop_words_{language}.txt')
-        except:
-            print(f'Creating stop_words_{language}.txt')
-        with open(f'filters/stop_words/stop_words_{language}.txt', 'w', encoding='utf-8') as f:
-            for word in stop_words:
-                f.write(f'{word}\n')
-        print(f'"{add_words_str}" {'has' if len(add_words) == 1 else 'have'} successfully been added to stop_words_{language}.txt')
+        add_stop_words()
 
 # allow for exiting the program
     elif choice == 'exit':
         exit()
 
 # handle invalid user input
-
     else:
         print('Invalid input.')
